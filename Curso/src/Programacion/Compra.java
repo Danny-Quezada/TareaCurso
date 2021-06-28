3d5f7149204e6f11956ceec1b68606b73067265e
Danny-Quezada <danny.quezada32u@std.uni.edu.ni>
2021-06-28 11:39:42 -0700

    Agregando las variables.
    Estas variables están con comentarios.

--- Curso/src/Programacion/Compra.java
+++ Curso/src/Programacion/Compra.java
@@ -1,5 +1,31 @@
 package Programacion;
 
+import java.util.Scanner;
+
 public class Compra {
 
+	static Scanner lector= new Scanner(System.in);
+	private double precio;
+	private double PrecioVenta;
+	private double dinero;
+	//Estas son las variables que sirven para determinar cual celular quiere comprar(depende de la marca)
+	private int q;
+	private int r;
+	private int e;
+	private int a;
+	//Termina hasta aqui.
+	
+	//Esta variable sirve para ver cuanto se ha ganado en la venta de celulares.
+	private double ganado;
+	//Termina hasta aqui
+	
+	//Esta varibale sirve para darle un fin a los ciclos de excepciones
+	private boolean salir = true;
+	
+	//Y por ultimo, estas variables son contadores de los celulares que vamos comprando(Depende del modelo)
+	private int n,n2,n3,n4,n5,n6, n7, n8, n9, n10, n11,n12,n13,n14,n15,n16, n17, n18, n19, n20, n21, n22, n23;
+	
+	
+	
+	
 }
