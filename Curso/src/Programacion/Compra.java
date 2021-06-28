0a030109167d3dd3250a708c10a3c8feb5c60985
Danny-Quezada <danny.quezada32u@std.uni.edu.ni>
2021-06-28 14:02:51 -0700

    Agregando los primeros métodos
    Escribiendo cuáles serán los métodos que hay que ocupar y los nombres de esos métodos.

--- Curso/src/Programacion/Compra.java
+++ Curso/src/Programacion/Compra.java
@@ -1,5 +1,6 @@
 package Programacion;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Compra {
@@ -25,7 +26,89 @@ public class Compra {
 	//Y por ultimo, estas variables son contadores de los celulares que vamos comprando(Depende del modelo)
 	private int n,n2,n3,n4,n5,n6, n7, n8, n9, n10, n11,n12,n13,n14,n15,n16, n17, n18, n19, n20, n21, n22, n23;
 	
-	
-	
-	
+	public void PedirDinero() {
+		int i = 0;
+		do {
+			try {
+				System.out.print("Cuanto dinero tienes? $"); 
+				dinero=lector.nextDouble();
+				if (dinero < 0) {
+					System.out.println("Cantidad de dinero incorrecta, el dinero debe de ser mayor a 0");
+					System.out.println();
+				} else {
+					salir = false;
+				}
+			} catch (InputMismatchException ex) {
+				System.out.println("Esta no es una respuesta válida, por favor intente de nuevo");
+				System.out.println();
+				lector.next();
+			}
+		} while (salir);
+		salir = true;
+		System.out.println();
+		do {
+			try {
+				System.out.println("Qué desea hacer?");
+				System.out.println("1. Comprar celulares");
+				System.out.println("2. Depositar más dinero");
+				System.out.print("Respuesta: ");
+				i = lector.nextInt();
+				if (i <= 0 || i > 2) {
+					System.out.println("Respuesta incorrecta, seleccione el número que corresponde a la opción que desea");
+					System.out.println();
+				} else {
+					salir = false;
+				}
+			} catch (InputMismatchException ex) {
+				System.out.println("Esta no es una respuesta válida, por favor intente de nuevo");
+				System.out.println();
+				lector.next();
+			}
+		} while (salir);
+		if(i==1) {
+			comprarAlgo();
+		}
+		else if(i==2) {
+			depositar();
+		}
+	}
+public void comprarAlgo() {
+		
+		int i = 0;
+		salir = true;
+		System.out.println();
+		do {
+			try {
+				System.out.println("Que celular quieres comprar?");
+				System.out.println("1. Iphones");
+				System.out.println("2. Samsung");
+				System.out.println("3. LG");
+				System.out.println("4. Huawei");
+				System.out.print("Respuesta: ");
+				i = lector.nextInt();
+				if (i <= 0 || i > 4) {
+					System.out.println("Respuesta incorrecta, seleccione el número que corresponde a la opción que desea");
+					System.out.println();
+				} else {
+					salir = false;
+				}
+			} catch (InputMismatchException ex) {
+				System.out.println("Esta no es una respuesta válida, por favor intente de nuevo");
+				System.out.println();
+				lector.next();
+			}
+		} while (salir);
+		if(i==1) {
+			iphones();
+		}
+		else if(i==2) {
+			samsung();
+		}
+		else if(i==3) {
+			LG();
+		}
+		else if(i==4) {
+			Huawei();
+		}
+	}
 }
