580c9fae11dd9f75b55783cec36b236d89bf2609
Hp Notebook <Hp Notebook@DESKTOP-93AN5N8>
2021-06-28 18:23:18 -0600

    En la linea 6 se tiene lo que se le agrego a la clase

--- Curso/src/Programacion/Main.java
+++ Curso/src/Programacion/Main.java
@@ -3,7 +3,7 @@ package Programacion;
 public class Main {
 
 	public static void main(String[] args) {
-		// Se realizo una instncia de la clase compra, y luego se llama al método para iniciar el programa
+		// Se realizo una instncia de la clase comprar y luego se llama al método para iniciar el programa
 		Compra Inicio = new Compra();
 		Inicio.PedirDinero();
 	}
