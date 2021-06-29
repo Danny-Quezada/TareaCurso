05fcf1fbc5887894341e805d9d560f3214a4aaab
Hp Notebook <Hp Notebook@DESKTOP-93AN5N8>
2021-06-28 18:30:20 -0600

    Se instancio la clase compra

--- Curso/src/Programacion/Main.java
+++ Curso/src/Programacion/Main.java
@@ -3,7 +3,7 @@ package Programacion;
 public class Main {
 
 	public static void main(String[] args) {
-		// Se realizo una instncia de la clase comprar y luego se llama al método para iniciar el programa
+		// Se realizo una instancia de la clase comprar y luego se llama al método para iniciar la app
 		Compra Inicio = new Compra();
 		Inicio.PedirDinero();
 	}
